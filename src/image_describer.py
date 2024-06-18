42277f5e244b96144a670e3b709e098b5bad12cb
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-06-18 14:35:36 +0200

    Completed image_describer.py. V3.1.

--- src/image_describer.py
+++ src/image_describer.py
@@ -271,11 +271,35 @@ class ImagesDescriber:
                     logger.error(f"An expected error occurred while moving {image_name}: {e}")
 
         # Display the images processing time
-        logger.info(
-            f"Processing complete: "
-            f"{processed_count} images processed, {unprocessed_count} unprocessed. "
-            f"Images processing time is {time.perf_counter() - time_start:.2f} seconds."
-        )
+        process_time = time.perf_counter() - time_start
+        if process_time < 60:
+            # Display process time in seconds
+            logger.info(
+                f"Processing complete: "
+                f"{processed_count} images processed, {unprocessed_count} unprocessed. "
+                f"Images processing time is {process_time:.2f} seconds."
+            )
+        elif 60 < process_time < 3600:
+            # Display process time in minutes and seconds
+            process_minutes = process_time // 60
+            process_seconds = process_time % 60
+            logger.info(
+                f"Processing complete: "
+                f"{processed_count} images processed, {unprocessed_count} unprocessed. "
+                f"Images processing time is {process_minutes:.0f} minutes {process_seconds:.0f} seconds."
+            )
+        else:
+            # Display process time in hours, minutes, and seconds
+            process_hours = process_time // 3600
+            process_minutes = (process_time % 3600) // 60
+            process_seconds = (process_time % 3600) % 60
+            logger.info(
+                f"Processing complete: "
+                f"{processed_count} images processed, {unprocessed_count} unprocessed. "
+                f"Images processing time is {process_hours:.0f} "
+                f"{'hour' if process_hours == 1 else 'hours'} "
+                f"{process_minutes:.0f} minutes {process_seconds:.0f} seconds."
+            )
 
     @staticmethod
     def remove_backup_file(filename):
