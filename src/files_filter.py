a12f428abfafa74991ed7f1083d62d797ec02763
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-06-22 17:01:12 +0200

    Modified filter_files.py

--- src/files_filter.py
+++ src/files_filter.py
@@ -2,22 +2,23 @@
 
 import os
 import shutil
-from src.logging_config import setup_logger
-from PIL import Image, JpegImagePlugin
 from pathlib import Path
-from typing import Optional, Union, List
+from typing import List, Union
+
+from PIL import Image, JpegImagePlugin
+
+from src.logging_config import setup_logger
 
 # Initialize logger using the setup function
 logger = setup_logger(__name__)
 
 
-def filter_files_by_extension(src_path: Union[str, List[str]], dst_path: Optional[str] = '') -> List[str]:
+def filter_files_by_extension(src_path: Union[str, List[str]]) -> List[str]:
     """
-    Filters files by image extensions. Non-image files are moved to a 'Not_Images folder.'
+    Filter files by image extensions. Non-image files are moved to a 'Not_Images' folder.
 
     Args:
         src_path: Path to source directory or a list of files.
-        dst_path: Destination directory for filtered files. Default to empty string.
 
     Returns:
         List of filtered image file names.
@@ -27,25 +28,25 @@ def filter_files_by_extension(src_path: Union[str, List[str]], dst_path: Optiona
     filtered_files = []
 
     for image_name in (
-            f for f in os.listdir(src_path)
-            if os.path.isfile(os.path.join(src_path, f)) and not f.startswith('.')
+        f
+        for f in os.listdir(src_path)
+        if os.path.isfile(os.path.join(src_path, f)) and not f.startswith('.')  # skip hidden files
     ):
         image_path = os.path.join(src_path, image_name)
-        destination_path = os.path.join(dst_path, image_name)
 
         # Check if the file is an image based on its extension
         if image_name.lower().endswith(
-                (
-                        '.jpg',
-                        '.jpeg',
-                        '.png',
-                        '.gif',
-                        '.bmp',
-                        '.webp',
-                        '.tiff',
-                        '.ico',
-                        '.svg',
-                )
+            (
+                '.jpg',
+                '.jpeg',
+                '.png',
+                '.gif',
+                '.bmp',
+                '.webp',
+                '.tiff',
+                '.ico',
+                '.svg',
+            )
         ) and os.path.isfile(image_path):
 
             with Image.open(image_path) as img:
@@ -53,11 +54,11 @@ def filter_files_by_extension(src_path: Union[str, List[str]], dst_path: Optiona
                 if isinstance(img, JpegImagePlugin.JpegImageFile):
                     filtered_files.append(image_name)
 
-                # Other image formats - move file without modification
+                # Other image formats - leave file without modification
                 else:
                     logger.warning(
                         f"Due to the unsupported ({str(image_name).upper().split('.')[-1]}) "
-                        f"format, the file '{image_name}' was left in the folder without being processed."
+                        f"format, the file '{image_name}' was left in the Source Folder without being processed."
                     )
 
         else:
