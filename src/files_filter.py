a3de066cd39ae2cf28e10b64f294a6e8b9963ad5
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-06-25 18:54:29 +0200

    Modified files_filter.py

--- src/files_filter.py
+++ src/files_filter.py
@@ -25,6 +25,7 @@ def filter_files_by_extension(src_path: Union[str, List[str]]) -> List[str]:
     """
     logger.info("Starting to filter files by image types in the source folder.")
 
+    image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.tiff', '.ico', '.svg'}
     filtered_files = []
 
     for image_name in (
@@ -35,21 +36,11 @@ def filter_files_by_extension(src_path: Union[str, List[str]]) -> List[str]:
         image_path = os.path.join(src_path, image_name)
 
         # Check if the file is an image based on its extension
-        if image_name.lower().endswith(
-            (
-                '.jpg',
-                '.jpeg',
-                '.png',
-                '.gif',
-                '.bmp',
-                '.webp',
-                '.tiff',
-                '.ico',
-                '.svg',
-            )
-        ) and os.path.isfile(image_path):
-
+        if image_name.lower().endswith(tuple(image_extensions)) and os.path.isfile(image_path):
             with Image.open(image_path) as img:
+                # Verify image integrity
+                img.verify()
+
                 # Ensure that only JPEG files are processed
                 if isinstance(img, JpegImagePlugin.JpegImageFile):
                     filtered_files.append(image_name)
@@ -57,36 +48,46 @@ def filter_files_by_extension(src_path: Union[str, List[str]]) -> List[str]:
                 # Other image formats - leave file without modification
                 else:
                     logger.warning(
-                        f"Due to the unsupported ({str(image_name).upper().split('.')[-1]}) "
-                        f"format, the file '{image_name}' was left in the Source Folder without being processed."
+                        f"Unsupported image format ({str(image_name).upper().split('.')[-1]}), "
+                        f"the file '{image_name}' was left in the source folder unprocessed."
                     )
-
         else:
-            # Move non-image files to the 'Not_Images' folder
-            try:
-                not_img_fld = os.path.join(src_path, 'Not_Images')
-                Path(not_img_fld).mkdir(parents=True, exist_ok=True)
+            move_non_image_format(image_name, image_path, src_path)
+        logger.info("Finished filtering files by image types in the source folder.")
+        return filtered_files
 
-                # Separate the filename and extension
-                base_name, ext = os.path.splitext(image_name)
-                counter = 1
-                file_to_move = image_name
-                destination_file_path = os.path.join(not_img_fld, file_to_move)
 
-                # Check if the file already exists in the destination folder
-                while os.path.exists(destination_file_path):
-                    file_to_move = f"{base_name}_({counter}){ext}"
-                    destination_file_path = os.path.join(not_img_fld, file_to_move)
-                    counter += 1
-                shutil.move(image_path, destination_file_path)
-                logger.warning(
-                    f"Moved non-image file '{image_name}' to {not_img_fld} as {file_to_move}."
-                )
-            except shutil.Error as e:
-                logger.error(f"Error moving file '{image_path}' to 'Not Images': {e}.")
-            except Exception as e:
-                logger.error(f"An expected error occurred while moving {image_name}: {e}")
+def move_non_image_format(image_name: str, image_path: str, src_path: str):
+    """
+    Move a non-image file to the 'Not_Images' folder, handling name conflicts.
 
-    logger.info("Finished filtering files by image types in the source folder.")
+    Args:
+        image_name (str): Name of the file to move.
+        image_path (str): Path of the file to move.
+        src_path (str): Source directory path.
 
-    return filtered_files
+    """
+    try:
+        not_img_fld = os.path.join(src_path, 'Not_Images')
+        Path(not_img_fld).mkdir(parents=True, exist_ok=True)
+
+        # Separate the filename and extension
+        base_name, ext = os.path.splitext(image_name)
+        destination_file_path = os.path.join(not_img_fld, image_name)
+
+        # Initialize counter for same file names
+        counter = 1
+
+        # Check if the file already exists in the destination folder
+        while os.path.exists(destination_file_path):
+            destination_file_path = os.path.join(not_img_fld, f"{base_name}_({counter}){ext}")
+            counter += 1
+
+        shutil.move(image_path, destination_file_path)
+        logger.warning(
+            f"Moved non-image file '{image_name}' to {not_img_fld} as {os.path.basename(destination_file_path)}."
+        )
+    except shutil.Error as e:
+        logger.error(f"Error moving file '{image_path}' to 'Not Images': {e}.")
+    except Exception as e:
+        logger.error(f"An expected error occurred while moving {image_name}: {e}")
