8a72c93b86d06f819abd3ab94377910447fe997b
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-09-26 12:54:34 +0200

    Update image_describer.py to integrate chatgpt_responder.py functionality

--- src/image_describer.py
+++ src/image_describer.py
@@ -1,13 +1,10 @@
-"""Describes images using OpenAI's GPT model and adds metadata."""
+"""Adds metadata to images."""
 
-import base64
 import os
 import shutil
-import sys
 import tempfile
 import time
 
-import requests
 from iptcinfo3 import IPTCInfo
 
 from src.check_access import terminate_processes_using_file
@@ -17,14 +14,6 @@ from src.response_parser import parse_response
 
 # Initialize logger using the setup function
 logger = setup_logger(__name__)
-
-# Set your OpenAI API key
-try:
-    with open('./openai_key.txt', 'r') as f:
-        API_KEY = f.read().strip()
-except FileNotFoundError as e:
-    logger.error(f"Could not find {e.filename}. Please ensure the file exists and is accessible.")
-    sys.exit(1)
 
 
 class ImagesDescriber:
@@ -42,13 +31,13 @@ class ImagesDescriber:
         __init__.py(prompt, src_path, image_files, dst_path):
             Initializes the ImagesDescriber with the specified parameters.
 
-        process_photo(image_file):
-            Processes a given image file to generate a title, description, and keywords using ChatGPT-4.
-            Returns a dictionary containing the generated title and keywords.
-
         add_metadata():
             Adds processed titles, descriptions, and keywords to the metadata of the images in the source directory.
             Saves the processed images in the destination directory.
+
+        remove_backup_file(filename):
+            Static method. Removes the backup file created by the IPTCInfo library, if it exists.
+            Typically, backup files have a '~' suffix.
 
         __str__():
             Returns a string representation of the ImagesDescriber instance.
@@ -69,51 +58,6 @@ class ImagesDescriber:
         self.image_files = image_files
         self.dst_path = dst_path if dst_path else src_path
         self.author_name = author_name
-
-    def process_photo(self, image_file):
-        """
-        Process a given photo to generate a title, description, and keywords with ChatGPT-4.
-
-        Args:
-            image_file (file object): The image file to be processed.
-
-        Returns:
-             dict: A JSON object containing the generated title, description, and keywords.
-        """
-        # Convert the image to a based64 string
-        image_base64 = base64.b64encode(image_file.read()).decode('ascii')
-
-        # Use GPT-4 by creating a prompt to generate a title, description, and keywords
-        payload = {
-            'model': 'gpt-4o',
-            'messages': [
-                {
-                    'role': 'user',
-                    'content': [
-                        {'type': 'text', 'text': self.prompt},
-                        {
-                            'type': 'image_url',
-                            'image_url': {'url': f"data:image/jpeg;base64,{image_base64}"},
-                        },
-                    ],
-                }
-            ],
-            'max_tokens': 300,
-        }
-
-        response = requests.post(
-            url="https://api.openai.com/v1/chat/completions",
-            headers={
-                'Content-Type': 'application/json',
-                'Authorization': f"Bearer {API_KEY}",
-            },
-            json=payload,
-        ).json()
-
-        # Get cleaned data from GPT response
-        title, description, keywords = parse_response(response)
-
-        return title, description, keywords
 
     def add_metadata(self):
         """
@@ -147,8 +91,10 @@ class ImagesDescriber:
 
             try:
                 with open(image_path, 'rb') as image_file:
-                    # Get data from GPT response
-                    title, description, keywords = self.process_photo(image_file=image_file)
+                    # Get cleaned data from GPT response
+                    title, description, keywords = parse_response(
+                        image_file=image_file, prompt=self.prompt
+                    )
 
                     # Create a temporary copy of the image
                     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
